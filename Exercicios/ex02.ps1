b28407c655a909d0974ca4e152947aa2f0399f78
ncarvalho05 <Nicole.Carvalho.T0132027@edu.atec.pt>
2026-02-23 10:06:51 +0000

    ex02.ps1

--- Exercicios/ex02.ps1
+++ Exercicios/ex02.ps1
@@ -1,18 +1,18 @@
-# Inicializar variaveis
+# Inicializar e declarar variaveis
 [int]$num1 = 0
 [int]$num2 = 0
 [int]$num3 = 0
 
-# Atribuir valores
-$num1 = Read-Host "Introduza o primeiro numero"
-$num2 = Read-Host "Introduza o segundo numero"
-$num3 = Read-Host "Introduza o terceiro numero"
+# Obter valores
+$num1 = Read-Host "Introduza num1"
+$num2 = Read-Host "Introduza num2"
+$num3 = Read-Host "Introduza num3"
 
 [object]$lista = @($num1, $num2, $num3)
 
-# Obter maior e menor
+# Identificar maior e menor
 [int]$maior = ($lista | Measure-Object -Maximum).Maximum
 [int]$menor = ($lista | Measure-Object -Minimum).Minimum
 
-Write-Host "O maior numero e: $maior"
-Write-Host "O menor numero e: $menor"
+Write-Host "Maior: $maior"
+Write-Host "Menor: $menor"
