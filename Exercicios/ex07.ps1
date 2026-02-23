0b7603dc3945c62a7c6fbfbd55c9fee16ed2a14c
ncarvalho05 <Nicole.Carvalho.T0132027@edu.atec.pt>
2026-02-23 10:10:02 +0000

    ex07.ps1

--- Exercicios/ex07.ps1
+++ Exercicios/ex07.ps1
@@ -1,1 +1,12 @@
+# Inicializar notas
+[double]$n1 = Read-Host "Nota 1 (Peso 2)"
+[double]$n2 = Read-Host "Nota 2 (Peso 3)"
+[double]$n3 = Read-Host "Nota 3 (Peso 5)"
 
+# Calcular media ponderada
+[double]$media = (($n1 * 2) + ($n2 * 3) + ($n3 * 5)) / 10
+
+Write-Host "Media: $media"
+
+if ($media -ge 6) { Write-Host "APROVADO" }
+else { Write-Host "REPROVADO" }
