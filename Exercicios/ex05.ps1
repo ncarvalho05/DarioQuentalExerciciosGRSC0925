6a63363a784011d0e445e2918d9298e1951fbf5b
ncarvalho05 <Nicole.Carvalho.T0132027@edu.atec.pt>
2026-02-23 10:08:38 +0000

    ex05.ps1

--- Exercicios/ex05.ps1
+++ Exercicios/ex05.ps1
@@ -1,1 +1,9 @@
+# Obter valores e criar objeto array
+[int]$v1 = Read-Host "num1"
+[int]$v2 = Read-Host "num2"
+[int]$v3 = Read-Host "num3"
+[object]$valores = @($v1, $v2, $v3)
 
+# Exibir ordenado
+Write-Host "Crescente: $(($valores | Sort-Object) -join ', ')"
+Write-Host "Decrescente: $(($valores | Sort-Object -Descending) -join ', ')"
