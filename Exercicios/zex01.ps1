b17a54f632e4c4c0a382e993658a246a9f957f83
ncarvalho05 <Nicole.Carvalho.T0132027@edu.atec.pt>
2026-02-23 10:01:56 +0000

    ex01.ps1

--- Exercicios/zex01.ps1
+++ Exercicios/zex01.ps1
@@ -4,13 +4,13 @@
 [int]$minutos = 0
 [int]$segundosRestantes = 0
 
-# Obter valor do terminal
+# Obter valor do terminal 
 $totalSegundos = Read-Host "Introduza o valor em segundos"
 
-# Calcular valores
+# Definir cálculos 
 $horas = [math]::Floor($totalSegundos / 3600)
 $minutos = [math]::Floor(($totalSegundos % 3600) / 60)
 $segundosRestantes = $totalSegundos % 60
 
-# Print do resultado 
-Write-Host "Resultado: $horas horas, $minutos minutos e $segundosRestantes segundos"
+# Exibir resultado 
+Write-Host "Saída esperada: $horas hora, $minutos minuto e $segundosRestantes segundos."
