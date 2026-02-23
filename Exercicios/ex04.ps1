d2ef402984e4bb04d8bf6c5ba9ffb97fae51f46f
ncarvalho05 <Nicole.Carvalho.T0132027@edu.atec.pt>
2026-02-23 10:07:57 +0000

    ex04.ps1

--- Exercicios/ex04.ps1
+++ Exercicios/ex04.ps1
@@ -1,1 +1,15 @@
+# Inicializar variaveis
+[double]$saldo = 0.0
+[double]$cheque = 0.0
 
+# Obter valores
+$saldo = Read-Host "Introduza o Saldo"
+$cheque = Read-Host "Introduza o valor do Cheque"
+
+# Logica de verificacao
+if ($saldo -ge $cheque) {
+    $saldo -= $cheque
+    Write-Host "Cheque descontado, saldo atualizado: $saldo"
+} else {
+    Write-Host "O cheque nao pode ser descontado por falta de saldo."
+}
