465241ed93344b61048f60ea0ad0ddce124028eb
ncarvalho05 <Nicole.Carvalho.T0132027@edu.atec.pt>
2026-02-23 10:09:25 +0000

    ex06.ps1

--- Exercicios/ex06.ps1
+++ Exercicios/ex06.ps1
@@ -1,1 +1,20 @@
+# Declarar variaveis
+[string]$nomeCliente = ""
+[double]$valorCompra = 0.0
+[double]$desconto = 0.0
+[double]$totalPagar = 0.0
 
+$nomeCliente = Read-Host "Nome do cliente"
+$valorCompra = Read-Host "Valor da compra"
+
+# Definir percentual de desconto
+if ($valorCompra -le 200) { $desconto = $valorCompra * 0.10 }
+elseif ($valorCompra -le 500) { $desconto = $valorCompra * 0.15 }
+else { $desconto = $valorCompra * 0.20 }
+
+$totalPagar = $valorCompra - $desconto
+
+Write-Host "Nome: $nomeCliente"
+Write-Host "Compra: $valorCompra €"
+Write-Host "Desconto: $desconto €"
+Write-Host "Total a pagar: $totalPagar €"
